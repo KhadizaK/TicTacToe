f5beb6e39e3f5cea1db2e396ae950e4c548ea860
Khadiza <khadiza.s.khanom@gmail.com>
2023-03-13 23:13:33 -0400

    Added the single player and asking for user names

--- TicTacgm.cpp
+++ TicTacgm.cpp
@@ -1,9 +1,14 @@
 #include <string>
 #include <iostream>
+#include <ctime>
 
 #define GRID_SIZE 3
 
 using namespace std;
+
+string playerONEName;
+string playerTWOName;
+char p1XorO, compXorO, p2XorO;
 
 class Game{
 
@@ -53,8 +58,18 @@ class Game{
                     }
                 }
                 if (winner) {
-                    puts("****** We have a winner!******");
-                    printf("Looks like %c won, congratulations!\n", previous_grid);
+                    printf("\n****** We have a winner!******\n");
+                    if (previous_grid == p1XorO) {
+                        cout << "Looks like " << playerONEName << ", " << previous_grid << ", won! Congradualtions!\n";
+                        
+                    } else if (previous_grid == p2XorO) {
+                        cout << "Looks like " << playerTWOName << ", " << previous_grid << ", won! Congradualtions!\n";
+                        
+                    } else {
+                        cout << "Looks like the computer, " << previous_grid << ", won! Congradualtions!\n";
+
+                    }
+                    //printf("Looks like %c won, congratulations!\n", previous_grid);
 
                     exit(0);
                     break;
@@ -101,7 +116,7 @@ class Game{
                         if (grid_position == 'X' || grid_position == 'O') {
                             puts("That grid position is already taken!");
                         } else {
-                            grid[row][col] = 'X';
+                            grid[row][col] = p1XorO;
                             break;
                         }
 
@@ -139,7 +154,7 @@ class Game{
                         if (grid_position == 'X' || grid_position == 'O') {
                             puts("That grid position is already taken!");
                         } else {
-                            grid[row][col] = 'O';
+                            grid[row][col] = p2XorO;
                             break;
                         }
 
@@ -168,7 +183,7 @@ class Game{
                     // this shows that positon is already taked so try another number
                 } else {
                     printf("Computer will play at %d.\n", computer_choice);
-                    grid[row][col] = 'O';
+                    grid[row][col] = compXorO;
                     break;
                 }
             }
@@ -197,34 +212,163 @@ class Game{
             }
         }
 
+        bool random_start(bool single_or_double) {
+            srand((unsigned) time(NULL));
+            int random = rand();
+            if (single_or_double) {
+                if (random % 2 == 0) {
+                    puts("Player 1 will go first.");
+                    return true;
+                    // player 1 starts
+                } else {
+                    puts("Player 2 will go first.");
+                    return false;
+                    // player two starts
+                }
+            } else {
+                if (random % 2 == 1) {
+                    puts("Player 1 goes first.");
+                    return true;
+                    // player one starts
+                } else {
+                    puts("Computer goes first.");
+                    return false;
+                    // computer starts
+                }
+            }
+        }
+        void choose_x_or_o(bool t_or_f, bool comp_or_player2) {
+            if (t_or_f) {
+                // if t_or_f is true it means that player one is chosen to go first
+                puts("What is your name player 1?");
+                cin >> playerONEName;
+                cout << "Welcome " << playerONEName << "!\n";
+                puts("Choose X or O");
+                cin >> p1XorO;
+                if (!comp_or_player2) {
+                    // this means that it's single player
+                    // it doesn't matter who goes first player 1 will always choose first
+                    if (p1XorO == 'X') {
+                        compXorO = 'O';
+                    } else if (p1XorO == 'O') {
+                        compXorO = 'X';
+                    }
+                } else {
+                    // this is when it's double player
+                    puts("What is your name player 2?");
+                    cin >> playerTWOName;
+                    cout << "Welcome " << playerTWOName << "!\n";
+                    if (p1XorO == 'X') {
+                        p2XorO = 'O';
+                        cout << playerTWOName << ", you are " << p2XorO << endl;
+                    } else if (p1XorO == 'O') {
+                        p2XorO = 'X';
+                        cout << playerTWOName << ", you are " << p2XorO << endl;
+                    }
+                }
+                
+            } else {
+                if(comp_or_player2) {
+                    // when comp_or_player2 is true that means it's double player
+                    // player two is chosen to go first
+                    puts("What is your name player 2?");
+                    cin >> playerTWOName;
+                    cout << "Welcome " << playerTWOName << "!\n";
+                    puts("Choose X or O");
+                    cin >> p2XorO;
+                    // gets name of player 1
+                    puts("What is your name player 1?");
+                    cin >> playerONEName;
+                    cout << "Welcome " << playerONEName << "!\n";
+                    
+                    if (p2XorO == 'X') {
+                        p1XorO = 'O';
+                        cout << playerONEName << ", you are " << p1XorO << endl;
+                    } else if (p2XorO == 'O') {
+                        p1XorO = 'X';
+                        cout << playerONEName << ", you are " << p1XorO << endl;
+                    }
+
+                }
+            }
+        }
+
         Game() {
             generate_grid();
+            bool single_or_double = ask_single_or_double_player();
 
-            if (ask_single_or_double_player()) {
-                ///
-                show_grid();
-                check_for_wins();
-                while (true) {
-                    ask_turn();
-                    check_for_wins();
+            if (single_or_double) {
+                /// true means it's multi player
+                if (random_start(single_or_double)) {
+                    // if player one is picked to go first
+                    bool t = true;
+                    choose_x_or_o(t, t);
                     show_grid();
-                    
-                    ask_turn_with_multiPlayer();
+                        check_for_wins();
+                        while (true) {
+                            ask_turn();
+                            check_for_wins();
+                            show_grid();
+                            
+                            ask_turn_with_multiPlayer();
+                            show_grid();
+                            check_for_wins();
+                        }
+
+                } else {
+                    // player two is picked to go first
+                    bool f = false;
+                    bool t = true;
+                    choose_x_or_o(f, t);
                     show_grid();
-                    check_for_wins();
+                        check_for_wins();
+                        while (true) {
+                            ask_turn_with_multiPlayer(); //ask_turn();
+                            check_for_wins();
+                            show_grid();
+                            
+                            ask_turn(); //ask_turn_with_multiPlayer();
+                            show_grid();
+                            check_for_wins();
+                        }
                 }
-
+                
             } else {
-                show_grid();
-                check_for_wins();
-                while (true) {
-                    ask_turn();
-                    check_for_wins();
-                    
-                    computer_player_turn();
+                // false when single player
+                if (random_start(single_or_double)) {
+                    // player 1 goes first
+                    bool f = false;
+                    bool t = true;
+                    choose_x_or_o(t, f);
                     show_grid();
                     check_for_wins();
+                    while (true) {
+                        ask_turn();
+                        check_for_wins();
+                        show_grid();
+                        
+                        computer_player_turn();
+                        show_grid();
+                        check_for_wins();
+                    }
+                } else {
+                    // Computer goes first
+                    bool f = false;
+                    bool t = true;
+                    choose_x_or_o(t, f);
+                    show_grid();
+                    check_for_wins();
+                    while (true) {
+                        computer_player_turn(); //ask_turn();
+                        check_for_wins();
+                        show_grid();
+                        
+                        ask_turn(); //computer_player_turn();
+                        show_grid();
+                        check_for_wins();
+                    }
                 }
+                
             }
             
         }
